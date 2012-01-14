ece39f7531417244ca80d0eb90958126d1f2efef
Tanner Danzey <arkaniad@gmail.com>
2012-01-14 15:08:40 -0600

    First commit. Plugin framework added, maven build files, etc.

---
+++ src/main/java/com/oei/chatng/PluginMain.java
@@ -0,0 +1,16 @@
+package com.oei.chatng;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class PluginMain extends JavaPlugin
+{
+
+	public void onDisable() {
+		
+	}
+
+	public void onEnable() {
+		
+	}
+	
+}
