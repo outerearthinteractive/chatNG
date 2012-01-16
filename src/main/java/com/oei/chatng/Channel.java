f28a3720169b814d561815d5b5232e4fe5dae5b6
Tanner Danzey <arkaniad@gmail.com>
2012-01-15 19:18:30 -0600

    Some weird channel stuff. Not sure what I'm doing right now.

--- src/main/java/com/oei/chatng/Channel.java
+++ src/main/java/com/oei/chatng/Channel.java
@@ -1,5 +1,32 @@
 package com.oei.chatng;
 
-public class Channel {
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Map.Entry;
 
+
+import org.bukkit.entity.Player;
+
+public class Channel {
+	private HashMap<String, Player> members;
+	private HashMap<String, Player> moderators;
+	private String name;
+	
+	public Channel(String name){
+		this.name = name;
+		initMaps();
+	}
+	public void addPlayerToChannel(Player player){
+		this.members.put(player.getName(), player);
+	}
+	private void initMaps(){
+		this.members = new HashMap();
+		this.moderators = new HashMap();
+	}
+	private void sendToAll(String message){
+		Iterator i = members.values().iterator();
+		while(i.hasNext()){
+		}
+	}
 }
